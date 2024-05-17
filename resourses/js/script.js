e035dd0e8ad944c145aade4a7c9039b25c984952
Nabeel Yaseen <nabeelyaseenggwp@gmail.com>
2024-05-17 23:56:29 +0500

    1.8

--- resourses/js/script.js
+++ resourses/js/script.js
@@ -1,1 +1,16 @@
-ReactDOM.render(<h1>Hello Nabeel!</h1>, document.querySelector("#root"));
+const rootEl = document.querySelector("#root");
+const listUl = (<ul>
+                    <li>Item 1</li>
+                    <li>Item 2</li>
+                </ul>);
+              
+// const reactRoot = ReactDOM.createRoot(rootEl);
+
+reactRoot.render(listUl)
+
+console.log(listUl)
+ReactDOM.render(
+    listUl, 
+    rootEl          
+);
+
